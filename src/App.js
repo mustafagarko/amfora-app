1601e34c708556ef8d84c889ce28c2866aa974fc
mustafagarko <keligov.m@mail.ru>
2023-09-14 08:35:34 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Homepage from "./pages/Homepage/Homepage";
 import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import ContactUs from "./pages/ContactUs/ContactUs";
 import ItemPage from "./pages/ItemPage/ItemPage";
@@ -14,7 +14,7 @@ function App() {
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/item/:uuid" element={<ItemPage />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import amforaLogo from "../assets/amforaLogo.png";
+import logo from "../assets/amforalogo.jpeg";
 
 function Footer() {
   return (
-    <div className="flex h-[25vh] absolute z-30 w-[100vw] bg-gray-500 justify-between">
+    <div className="flex h-[25vh] absolute z-30 w-[100vw] bg-[#D2B08B] justify-between mt-[5vh]">
       <div className=" p-4 flex flex-col justify-between">
-        <h1 className="text-white font-thin text-2xl">Amfora LLC</h1>
+        <div className="flex gap-4">
+          <h1 className="text-white font-thin text-2xl">Amfora LLC</h1>
+          <img src={logo} className="w-8 h-8 -rotate-45" />
+        </div>
         <h1 className="text-white font-thin text-xl">
           Phone: +1 (234) 567 890
         </h1>
@@ -12,7 +16,7 @@ function Footer() {
           All rights reserved tuda syuda
         </h1>
       </div>
-      <img src={amforaLogo} className="w-36 p-4 h-full" />
+      <img src={amforaLogo} className="w-36 p-4 h-full mr-8" />
     </div>
   );
 }
